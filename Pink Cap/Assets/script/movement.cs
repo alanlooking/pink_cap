e77faf9453f1b82cb30bf4093ba7634834df77b1
Feokhard <khalilov.11edem@gmail.com>
2026-07-03 16:22:44 +0300

    Добавил анимацию и перемешение

--- Pink Cap/Assets/script/movement.cs
+++ Pink Cap/Assets/script/movement.cs
@@ -4,23 +4,19 @@ public class movement : MonoBehaviour
 {
     public float speed = 5f;
     public float jump = 7f;
+
     private Rigidbody2D rb;
-
-
+    private Animator animator;
+    private bool isGrounded = false;
 
     void Start()
     {
-        
-
-        rb =  GetComponent<Rigidbody2D>();
-
-
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-
 
     void Update()
     {
-
         float move = 0f;
 
         if (Input.GetKey(KeyCode.D))
@@ -32,16 +28,34 @@ public class movement : MonoBehaviour
         {
             move = -1f;
         }
+
         rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
 
-
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
-
+            isGrounded = false;
         }
 
+        bool isMoving = move != 0f;
+        bool isJumping = !isGrounded;
 
+        animator.enabled = !isMoving && !isJumping;
+    }
 
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
