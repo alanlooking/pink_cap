f9537acc00efefbddb3bae0a4e09ba1144abca7c
Alan <alan.omarov.14@mail.ru>
2026-07-05 21:18:30 +0300

    Подкорректировал уровни, добавил экран смерти

--- Pink Cap/Assets/script/Player/playerDeath.cs
+++ Pink Cap/Assets/script/Player/playerDeath.cs
@@ -11,13 +11,18 @@ public class PlayerDeath : MonoBehaviour
     [SerializeField] private CanvasGroup fadeCanvasGroup;
     [SerializeField] private Text finalLine1; 
     [SerializeField] private Text finalLine2; 
+    [SerializeField] private string mainMenuSceneName = "MainMenu";
+
+    [Header("Эффект обычной смерти")]
+    [SerializeField] private RectTransform deathCircleRect;
+    [SerializeField] private float circleExpandSpeed = 20f; 
 
     private bool isDead = false;
-
     private Rigidbody2D rb;
     private Collider2D playerCollider;
     private movement movementScript;
     private Animator animator;
+    private Camera mainCamera;
 
     void Start()
     {
@@ -25,9 +30,9 @@ public class PlayerDeath : MonoBehaviour
         playerCollider = GetComponent<Collider2D>();
         movementScript = GetComponent<movement>();
         animator = GetComponent<Animator>();
+        mainCamera = Camera.main;
     }
 
-    
     public void Die(bool isFinalDeath = false)
     {
         if (isDead)
@@ -51,20 +56,52 @@ public class PlayerDeath : MonoBehaviour
 
         if (isFinalDeath)
         {
-            
             StartCoroutine(FinalCutsceneSequence());
         }
         else
         {
             
-            Destroy(gameObject, 0.65f);
-            Invoke(nameof(RestartLevel), restartDelay);
+            StartCoroutine(DeathCircleSequence());
         }
+    }
+
+    private IEnumerator DeathCircleSequence()
+    {
+        
+        
+        yield return new WaitForSeconds(0.65f);
+
+        
+        if (deathCircleRect != null && mainCamera != null)
+        {
+            
+            Vector2 screenPosition = mainCamera.WorldToScreenPoint(transform.position);
+
+            
+            deathCircleRect.position = screenPosition;
+            deathCircleRect.localScale = Vector3.zero;
+            deathCircleRect.gameObject.SetActive(true);
+
+            
+            float currentScale = 0f;
+            
+            while (currentScale < 35f)
+            {
+                currentScale += circleExpandSpeed * Time.deltaTime;
+                deathCircleRect.localScale = new Vector3(currentScale, currentScale, 1f);
+                yield return null;
+            }
+        }
+
+        
+        yield return new WaitForSeconds(0.2f);
+
+        
+        RestartLevel();
     }
 
     private IEnumerator FinalCutsceneSequence()
     {
-        
         float time = 0;
         float fadeDuration = 1.5f;
         while (time < fadeDuration)
@@ -80,25 +117,23 @@ public class PlayerDeath : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);
 
-        
         if (finalLine1 != null)
         {
             finalLine1.text = "You were my favorite android...";
             finalLine1.gameObject.SetActive(true);
         }
 
-        
         yield return new WaitForSeconds(2.0f);
 
-        
         if (finalLine2 != null)
         {
             finalLine2.text = "...until you get out of hand.";
             finalLine2.gameObject.SetActive(true);
         }
 
-         
-        
+        yield return new WaitForSeconds(4.0f);
+
+        SceneManager.LoadScene(mainMenuSceneName);
     }
 
     private void RestartLevel()
